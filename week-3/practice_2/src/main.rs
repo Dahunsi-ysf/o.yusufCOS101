a11a2db0e52fad42c51f70cc935829112b6a2ca5
Dahunsi-ysf <oluwadahunsi.yusuf@pau.edu.ng>
2025-10-20 00:15:35 +0100

    (week-3)

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let result = 10;           // i32 by default
+    let age: u32 = 20;         // unsigned 32-bit integer
+    let sum: i32 = 5 - 15;     // signed 32-bit integer
+
+    println!("Result value is {}", result);
+    println!("Age is {}", age);
+    println!("Sum is {}", sum);
+}
