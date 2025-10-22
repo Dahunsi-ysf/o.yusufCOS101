c04a7036281dd8c065b6487d27f7a24d17d0debc
Dahunsi-ysf <oluwadahunsi.yusuf@pau.edu.ng>
2025-10-22 23:12:19 +0100

    (week-4)

---
+++ week-4/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\HP\o.yusufCOS101\week-4\practice_4\target\debug\practice_4.exe: C:\Users\HP\o.yusufCOS101\week-4\practice_4\src\main.rs
---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\HP\o.yusufCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\HP\o.yusufCOS101\week-4\practice_6\src\main.rs
